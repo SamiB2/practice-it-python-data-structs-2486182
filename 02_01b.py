5625ea67f6838654de64826c63323518e10671ff
Sami Bounouar <92109469+SamiB2@users.noreply.github.com>
2025-11-04 11:08:28 +0000

    Exo 2

--- 02_01b.py
+++ 02_01b.py
@@ -1,7 +1,14 @@
+from collections import deque 
 
 def main():
-    #add code here
-    return
+    foods = deque(maxlen=5)
+    foods.append("STA001") # we can append one at a time
+    ordered_foods = ["DES003", "STA002", "ENT004", "ENT001"]
+    foods.extend(ordered_foods)
+    foods.append("DES002")
+    print(foods)
+    
+    return 
 
 if __name__ == "__main__":
     main()
