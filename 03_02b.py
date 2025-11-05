aa3a098669d0a82e3302c2abc373859cef84cca3
Sami Bounouar <92109469+SamiB2@users.noreply.github.com>
2025-11-05 13:34:45 +0000

    Exo 3

--- 03_02b.py
+++ 03_02b.py
@@ -1,8 +1,15 @@
+import csv
+from collections import namedtuple
+
 
 def main():
-    #add code here
-    #read data/Customer.csv
-    #Create workable objects with each line
+    with open('/workspaces/practice-it-python-data-structs-2486182/data/Customer.csv', 'r') as f:
+        reader = csv.reader(f)
+        headers = next(reader)
+        Row = namedtuple('Row', headers)
+        for r in reader:
+            row = Row(*r)
+            print(row)
     return
 
 if __name__ == "__main__":
