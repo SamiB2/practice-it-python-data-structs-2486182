b0b51e492e10c8031ddcb13a654e0f0726d2fdd2
Sami Bounouar <92109469+SamiB2@users.noreply.github.com>
2025-11-06 13:21:44 +0000

    EXO 4

--- 04_01b.py
+++ 04_01b.py
@@ -1,7 +1,26 @@
 from collections import namedtuple
+from collections import defaultdict
+from pprint import pprint
+
+def get_dict(list_to_categorise):
+    res=defaultdict(lambda: set())
+    for item in list_to_categorise:
+        cat = item.identifier[0:3]
+        match cat:
+            case "STA":
+                res["starter"].add(item)
+            case "BEV":
+                res["beverage"].add(item)
+            case "SAL":
+                res["salad"].add(item)
+            case "ENT":
+                res["entree"].add(item)
+            case "DES":
+                res["dessert"].add(item)
+    return res
+
 
 def main():
-    #add code here
     Food = namedtuple("Food", ["identifier", "name"])
 
     nadias_list = [
@@ -35,8 +54,9 @@ def main():
         Food("DES005",	"Mixed Berry Tart"),
         Food("BEV003",	"Cafe Latte"),
     ]
-
+    pprint(dict(get_dict(nadias_list)))
     return
+
 
 if __name__ == "__main__":
     main()
--- 04_02b.py
+++ 04_02b.py
@@ -1,9 +1,22 @@
 from collections import namedtuple
+from collections import defaultdict
+from pprint import pprint
+from csv import reader
+
 
 def main():
     #add code here
-
+    res = defaultdict(int)
+    with open("/workspaces/practice-it-python-data-structs-2486182/data/OrderItems.csv", "r") as open_csv:
+        read = reader(open_csv)
+        Item = namedtuple("Item", next(read))
+        for line in read:
+            item = Item(*line)
+            res[item.ProductID] += int(item.Quantity)
+    pprint(dict(res))
     return
+
+
 
 if __name__ == "__main__":
     main()
